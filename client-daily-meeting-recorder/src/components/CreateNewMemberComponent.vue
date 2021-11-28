91b802b1077fdd42570cb171361e8624a0b679f3
Antonin CHATEIGNER <antonin.chateigner@diiage.org>
2021-11-29 00:02:13 +0100

    maj projectSelected

---
+++ client-daily-meeting-recorder/src/components/CreateNewMemberComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <form @submit="addNewMember">
+        <div>
+            <input type="email" placeholder="email" name="email" id="email" v-model="emailMember">        
+            <input class="btnValide" type="submit" value="Validate">
+        </div>
+    </form>
+    </div>
+</template>
+
+<script>
+//import createNewMember from '../components/CreateNewMemberComponent.vue';
+
+export default {
+    name: 'createMember',
+    components: {
+        
+    },
+    data() {
+        return {
+           emailMember: ''
+        }
+    },
+    mounted(){
+    
+    },
+    methods: {
+        addNewMember(){
+
+        }
+
+    }    
+    
+}
+</script>
--- client-daily-meeting-recorder/src/views/ProjectSelected.vue
+++ client-daily-meeting-recorder/src/views/ProjectSelected.vue
@@ -1,35 +1,46 @@
 <template>
     <div class="bodyComponent">
         <form @submit="modifProject">
-        <div class="FormAddProject">
-            <label for="titleProject" class="labelProject">Title</label>
-            <input type="text" disabled placeholder="Project Title" name="titleProject" id="titleProject" v-model="titleProject">
-            <label for="descriptionProject" class="labelProject">Description</label>
-            <textarea disabled placeholder="Project Desciption" name="descriptionProject" id="descriptionProject" v-model="descriptionProject"></textarea>   
-            <label for="" class="labelProject">Status</label> 
-            <select disabled name="StatusProject" id="statusProject">
-                <option value="">1</option>
-                <option value="">2</option>
-                <option value="">3</option>
-            </select>     
-            <div>
-                <input class="btnValide" type="submit" value="Validate">
-                <input class="btnValide" type="submit" value="Annuler">
-            </div>
-        </div>
-    </form>
-
-    <div>
-        <form @submit="createNewProject" v-if="projectCreated === false">
-            <div id="createNewProject">
-                <input class="btnComponent" type="submit" value="Modifier">
-                <input class="btnComponent" type="submit" value="Supprimer">
+            <div class="FormAddProject">
+                <label for="titleProject" class="labelProject">Title</label>
+                <input type="text" disabled placeholder="Project Title" name="titleProject" id="titleProject" v-model="titleProject">
+                <label for="descriptionProject" class="labelProject">Description</label>
+                <textarea disabled placeholder="Project Desciption" name="descriptionProject" id="descriptionProject" v-model="descriptionProject"></textarea>   
+                <label for="" class="labelProject">Status</label> 
+                <select disabled name="StatusProject" id="statusProject">
+                    <option value="">1</option>
+                    <option value="">2</option>
+                    <option value="">3</option>
+                </select>     
+                <div>
+                    <input class="btnValide" type="submit" value="Valider">
+                    <input class="btnValide" type="submit" value="Annuler">
+                </div>
             </div>
         </form>
-    </div>
 
+        <div>
+            <form @submit="modifProject" v-if="projectModif === false">
+                <div id="createNewProject">
+                    <input class="btnValide" type="submit" value="Modifier projet">
+                    <input class="btnValide" type="submit" value="Supprimer projet">
+                </div>
+            </form>
+        </div>
 
+        <hr/>
 
+        <div>
+            <input class="btnValide" v-if="manageMember === false" type="submit" value="Gérer membre">
+            <input class="btnValide" v-if="manageMember === true" type="submit" value="cacher membre">
+            <input class="btnValide" v-if="manageMeeting === false" type="submit" value="Gérer meeting">
+            <input class="btnValide" v-if="manageMeeting === true" type="submit" value="cacher meeting">
+        </div>
+
+        <hr/>
+
+        <manageMember v-if="manageMember === true"></manageMember><!-- components -->
+        <manageMeeting v-if="manageMeeting === true"></manageMeeting><!-- components -->
 
     </div>
 </template>
@@ -39,16 +50,20 @@
 </style>
 
 <script>
-// import { defineComponent } from '@vue/composition-api'
+import manageMember from '../components/ManageMemberComponent.vue';
+import manageMeeting from '../components/ManageMeetingComponent.vue';
 
 export default({
     name:'ProjectSelected',
     components: {
-        
+        manageMember,
+        manageMeeting
     },
     data(){
         return{
-            
+            projectModif: false,
+            manageMember: false,
+            manageMeeting: false
         }
     },
     mounted(){
