0ff4ceb5b7362ceaff4df4d4193ed762f860d31c
= <=>
2021-11-29 16:19:28 +0100

    Audio recording and saving on the client

--- client-daily-meeting-recorder/src/router/index.js
+++ client-daily-meeting-recorder/src/router/index.js
@@ -5,6 +5,7 @@ import Login from '../views/Login.vue'
 import Meeting from '../views/Meeting.vue'
 import Project from '../views/Project.vue'
 import ProjectSelected from '../views/ProjectSelected.vue';
+import RecordingComponent from '../components/RecordingComponent'
 
 Vue.use(VueRouter)
 
@@ -38,6 +39,11 @@ const routes = [
     path: '/projectSelected',
     name: 'ProjectSelected',
     component: ProjectSelected
+  },
+  {
+    path : '/record',
+    name : 'RecordManager',
+    component : RecordingComponent
   }
 ]
 
