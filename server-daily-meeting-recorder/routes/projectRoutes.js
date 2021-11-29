785008ae7a54cae80339e7074c8ea6a796309f87
Antonin CHATEIGNER <antonin.chateigner@diiage.org>
2021-11-29 16:54:33 +0100

    maj

--- server-daily-meeting-recorder/routes/projectRoutes.js
+++ server-daily-meeting-recorder/routes/projectRoutes.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 const projectController = require('../controllers/projectController');
 
-router.post('/createProject', projectController.createProject);
+router.post('/createProject', passport.authenticate('jwt', { session: false }),projectController.createProject);
 router.get('/getAllUserProjects', projectController.getAllUserProjects);
 router.post('/addProjectToTeam', projectController.addProjectToTeam);
 router.get('/getProjectMeetings', projectController.getProjectMeetings);
